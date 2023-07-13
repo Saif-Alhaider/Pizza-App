1960d7c972f2c72608c58e4dd3dda31228f29beb
Saif Alhaider <saifharith90@gmail.com>
2023-07-13 10:07:40 +0300

    refactor: removed unnecessary function

--- app/src/main/java/com/example/pizzaapp/MainActivity.kt
+++ app/src/main/java/com/example/pizzaapp/MainActivity.kt
@@ -32,19 +32,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PizzaAppTheme {
-        Greeting("Android")
-    }
-}
